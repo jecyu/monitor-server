aa3230241142768a6c99eda712662e0424f7aef8
jecyu <linjiyu@sf-express.com>
2024-06-24 22:58:46 +0800

    feat: 添加错误上报记录

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,9 +9,29 @@ export class AppController {
     return { message: 'Data saved' };
   }
 
+  @Post('error-report')
+  reportError(@Body() body: any) {
+    // TODO 后续支持多个错误信息
+    // const errors = fs.existsSync('errors.json')
+    //   ? JSON.parse(fs.readFileSync('errors.json', 'utf-8'))
+    //   : [];
+    // errors.push(body);
+    const errors = [body];
+    fs.writeFileSync('errors.json', JSON.stringify(errors));
+    return { message: 'Error reported' };
+  }
+
   @Get('events')
   getEvents() {
     const data = fs.readFileSync('data.json', 'utf-8');
+    return JSON.parse(data);
+  }
+
+  @Get('errors')
+  getErrors() {
+    const data = fs.existsSync('errors.json')
+      ? fs.readFileSync('errors.json', 'utf-8')
+      : '[]';
     return JSON.parse(data);
   }
 }
